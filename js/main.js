5d67547c02240a940264d46cf39dfcfae04263e2
Ko3uHaK <den2001888@yandex.by>
2021-09-28 16:41:16 +0300

    start work

---
+++ js/main.js
@@ -0,0 +1,87 @@
+// function showTime(speed, distance){
+//     let time = speed / distance;
+//     return time;
+// }
+// let myTime = showTime(200, 12);
+// console.log(myTime);
+
+// let i = 12;
+// function myBuget(money, rasxod, pribil){
+//     let buget = money - rasxod + pribil;
+//     return buget;
+// }
+// myBuget(100, 12, 2);
+// if (i>myBuget){
+//     console.log("fdkf");
+// }else{
+//     console.log("dsd");
+// }
+
+// let safeToProceed = false;
+//  if (safeToProceed) {
+//  alert("You shall pass!");
+//  } else {
+//  alert("You shall not pass!");
+//  }
+// var color = prompt("Введите цвет", color);
+// switch (color) {
+//   case "green":
+//     alert("green color");
+//     break;
+//   case "black":
+//     alert("black color");
+//     break;
+//   default:
+//     alert("no color");
+//     break;
+// }
+/*
+for(let i = 0; i < 10;i++){
+  saySomething();
+}
+function saySomething(){
+  document.writeln('sadasdasd');
+}*/
+// function dontSleep(){
+//   alert('вы еще сдесь ?');
+// }
+// let timeID = setTimeout(dontSleep, 10000);
+// function youSayGoodBye() {
+//   alert("Good Bye!");
+//   function andISayHello() {
+//   alert("Hello!");
+//   }
+//   return andISayHello;
+//  }
+//  let something = youSayGoodBye();
+//  something();
+// function stopWatch() {
+//   var startTime = Date.now();
+
+//   function getDelay() {
+//   var elapsedTime = Date.now() - startTime;
+//   alert(elapsedTime);
+//   }
+//   return getDelay;
+//   }
+//   let timer = stopWatch();
+//   // Сделать что-нибудь за некоторое время.
+//   for (let i = 0; i < 1000000; i++) {
+//   let foo = Math.random() * 10000;
+//   }
+//   // Вызвать возвращаемую функцию.
+//   timer();
+// let good = ["Mario", "Luigi", "Kirby", "Yoshi"];
+// let bad = ["Bowser", "Koopa Troopa", "Goomba"];
+// let goodAndBad = good.concat(bad);
+// console.log(goodAndBad);
+
+// 
+// 
+// let numbers =[1,2,3,4,5,6,7,8,9,10,11,12,];
+// let total = numbers.reduce(function(total, current) {
+//   return total + current;
+//  }, 0);
+//  console.log(total);
+const number = "24";
+console.log(+number + 23);
