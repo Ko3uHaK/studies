cbe895d69d0acab63dd87ad6bd6f887d79ae30ff
Ko3uHaK <den2001888@yandex.by>
2021-10-22 21:10:26 +0300

    arr 3

--- js/main.js
+++ js/main.js
@@ -301,3 +301,14 @@ function saySomething(){
 // arr[2] = 3;
 // console.log(arr)
 //3
+// const arr =[1, 2 ,3]
+// arr[0]+=3;
+// arr[1]+=3;
+// arr[2]+=3;
+// console.log(arr)
+// const arr =[1, 2 ,3]
+// arr[0]++
+// ++arr[1]
+// arr[2]++
+// console.log(arr)
+//4
