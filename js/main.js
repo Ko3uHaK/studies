54033e9d57d534ae7aa9b3f475950327d8765973
Ko3uHaK <den2001888@yandex.by>
2021-10-23 13:43:53 +0300

    end practic

--- js/main.js
+++ js/main.js
@@ -287,27 +287,18 @@ function saySomething(){
 // }else{
 //   alert('Вам нельзя покупать алкоголь!')
 // }
-const h2 = document.querySelector(".h2");
-function clickHandler(){
-  const currentColor = h2.style.color;
-  if(currentColor === a){
-    h2.style.color = b;
-  }else{
-    h2.style.color = a;
-  }
-} 
-const a = 'rgb(85, 102, 85)';
-const b = "#575fcf";
-function init(){
-  h2.style.color = a;
-  h2.addEventListener("mouseenter",clickHandler);
-}
-init();
+// const h2 = document.querySelector(".h2");
+// const clicked = 'clicked';
 
-function offlineHandler(){
-  console.log('goodbye');
-}
-window.addEventListener("offline",offlineHandler);
+
+// function clickHandler(){
+//   h2.classList.toggle(clicked);
+// }
+
+// function init(){
+//   h2.addEventListener("click",clickHandler);
+// }
+// init();
 
 //Массивы
 //1
