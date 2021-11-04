5f25002f43291c57c9c06e48228618e1a160cf48
Ko3uHaK <den2001888@yandex.by>
2021-11-04 12:46:50 +0300

    delete 1

--- js/main.js
+++ js/main.js
@@ -469,3 +469,8 @@ function saySomething(){
 // let obj = {a: 1, b: 2, c: 3, d: 4, e: 5};
 // console.log('e' in obj);
 // console.log('f' in obj);
+//Оператор delete
+//1
+// let obj = {a: 1, b: 2, c: 3, d: 4, e: 5};
+// delete obj.e;
+// console.log('e' in obj);
