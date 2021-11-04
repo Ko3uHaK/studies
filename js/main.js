649cfb34ce96cf3d69c6569d35a4ad2986b72ee6
Ko3uHaK <den2001888@yandex.by>
2021-11-04 12:38:37 +0300

    peredacha 3

--- js/main.js
+++ js/main.js
@@ -449,10 +449,18 @@ function saySomething(){
 // arr1[0] = 'a';
 // console.log(arr2);
 //2
+// let arr1 = [1, 2, 3];
+// let arr2 = arr1;
+
+// arr1[0] = 'a';
+// arr2[1] = 'b';
+
+// console.log(arr1);
+//3
 let arr1 = [1, 2, 3];
 let arr2 = arr1;
 
 arr1[0] = 'a';
-arr2[1] = 'b';
+arr2[0] = 'b';
 
-console.log(arr1);
+console.log(arr2);
